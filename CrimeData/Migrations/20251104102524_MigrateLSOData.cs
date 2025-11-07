dec4dc46a1c1b64bbbb36b498a54e851b4cf417b
mkb <michael.bourke@live.co.uk>
2025-11-07 11:53:46 +0000

    ss

--- CrimeData/Migrations/20251104102524_MigrateLSOData.cs
+++ CrimeData/Migrations/20251104102524_MigrateLSOData.cs
@@ -29,7 +29,16 @@ select  icd.CrimeID,ct.Value as CrimeType,icd.Longitude,icd.Latitude,loc.Value a
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql(@"
+DROP VIEW if exists CrimeDataReadable
+go
+create view CrimeDataReadable as
+select  icd.CrimeID,ct.Value as CrimeType,icd.Longitude,icd.Latitude,loc.Value as location,fw.Value as FallsWithin,rb.Value as ReportedBy,m.Value as Month from ImportCrimeData icd
+    left outer join Authorities rb on rb.Id = icd.ReportedById
+    left outer join Location loc on loc.id = icd.LocationId
+        left outer join Authorities fw on fw.Id = icd.FallswithinId
+    inner join month m on m.Id = icd.MonthId
+           left outer join CrimeType ct on ct.Id  = icd.CrimeTypeId");
         }
     }
 }
--- CrimeData/Migrations/20251104111249_MigrateData.cs
+++ CrimeData/Migrations/20251104111249_MigrateData.cs
@@ -30,7 +30,18 @@ select  icd.CrimeID,ct.Value as CrimeType,icd.Longitude,icd.Latitude,loc.Value a
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql(
+                @"DROP VIEW if exists CrimeDataReadable 
+go
+create view CrimeDataReadable as
+select  icd.CrimeID,ct.Value as CrimeType,icd.Longitude,icd.Latitude,loc.Value as location,fw.Value as FallsWithin,rb.Value as ReportedBy,m.Value as Month,lc.Value as lsoaCode,ln.Value as lsoaName from ImportCrimeData icd
+    left outer join Authorities rb on rb.Id = icd.ReportedById
+    left outer join Location loc on loc.id = icd.LocationId
+        left outer join Authorities fw on fw.Id = icd.FallswithinId
+    left outer join LSOAcode lc on lc.id = icd.LSOAcodeId
+    left outer join LSOAName ln on ln.id = icd.LSOAnameId
+    inner join month m on m.Id = icd.MonthId
+           left outer join CrimeType ct on ct.Id  = icd.CrimeTypeId");
         }
     }
 }
